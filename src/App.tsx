1438fa3e8758e3802f48f19dbbe8a03bcd924771
JoaoBianco <pepe.bianco@hotmail.com>
2023-03-15 16:20:16 -0300

    Create Country page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import "./css/resetCss.scss";
 import "./css/style.scss";
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/Main";
+import Country from "./pages/Country";
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+        <Route path="/:country" element={<Country />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
